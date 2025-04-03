a0c8f8e3f3fd93512b0a73c36e6db07c7f31c503
Gino Latorilla <pblatorilla+github@gmail.com>
2025-04-03 12:34:37 +0800

    Serve the registry securely

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+set -euo pipefail
+
 PROJECT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 VENV_DIR="${PROJECT_DIR}/.venv"
 ANSIBLE_DIR="${PROJECT_DIR}/ansible"
 
 if [ ! -d "$VENV_DIR" ]; then
     python3 -m venv "$VENV_DIR"
-    "$VENV_DIR"/bin/pip install -r "$ANSIBLE_DIR"/requirements.txt
 fi
 
 source "$VENV_DIR"/bin/activate
+pip install -r "$ANSIBLE_DIR"/requirements.txt
+ansible-galaxy collection install -r "$ANSIBLE_DIR"/requirements.yaml
 ansible-playbook "$ANSIBLE_DIR"/playbook.yaml -i "$ANSIBLE_DIR"/inventory.ini "$@"

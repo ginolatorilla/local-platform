838dbf3cea6702c0075e3c669bc3b2879cc45e0a
Gino Latorilla <pblatorilla+github@gmail.com>
2026-03-13 23:51:21 +0800

    VM reset script

---
+++ reset-vm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+PROJECT_DIR="$(readlink -f $(dirname "${BASH_SOURCE[0]}"))"
+
+echo '=== 👀 Checking if required tools are installed...'
+for tool in kubectl helm lima docker skopeo htpasswd; do
+  if command -v "$tool" > /dev/null; then
+    echo "--- ✅ $tool is installed"
+  else
+    echo "--- ❌ $tool is not installed"
+    exit 1
+  fi
+done
+
+limactl stop k8s --tty=false -f
+cp -f $PROJECT_DIR/k8s.lima.yaml ~/.lima/k8s/lima.yaml
+limactl start --name k8s --tty=false
